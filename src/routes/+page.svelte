ac4dba8f4f1b3cedb9b8a71553121ec0fd4edf8a
M-Hidayatullah <hidayatullahblk@gmail.com>
2024-02-17 18:55:16 +0800

    components tailwind, state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1 class="text-green-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Conversion from "../components/Conversion.svelte";
+    import FaQs from "../components/FAQs.svelte";
+    import Hero from "../components/Hero.svelte";
+    import Product from "../components/Product.svelte";
+    import Reviews from "../components/Reviews.svelte";
+</script>
+
+<main class="flex flex-col">
+    <Hero />
+    <Product />
+    <Reviews />
+    <FaQs />
+    <Conversion />
+</main>
